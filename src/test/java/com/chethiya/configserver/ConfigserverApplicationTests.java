bcf8f4f4c78f870b6d9a129531f6c2e86dbeccee
Chethiya Tharinda Palliyaguruge <ctharinda4114@gmail.com>
2023-10-05 14:45:40 +1300

    Configs server initial commit

---
+++ src/test/java/com/chethiya/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chethiya.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
